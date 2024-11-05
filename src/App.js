b9ca4c16481d1e0c20c7aacdbd09ead4bd8c606d
Ayush <heyayush87@gmail.com>
2024-11-05 22:56:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-400 justify-center m-auto">
+      <h1> working</h1>
+    </div>
+  );
+}
+
+export default App;
